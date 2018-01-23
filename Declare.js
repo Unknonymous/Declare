15e2d632400725b50c963f4083b87ae7e1d8b75f
Ray Lee <Lee31978@gmail.com>
2018-01-22 22:11:48 -0500

    This is the first and final commit for the 'I do declare' assignment

---
+++ Declare.js
@@ -0,0 +1,23 @@
+var pi = 3.14159265359;
+var phone = 5176952035;
+var favorite = 3;
+var root = 1.41421356237;
+
+var string = "If I'm a string, does that mean I can twist myself into yarn?";
+var string2 = "I wanna cookie!";
+var yarn = "I finally made it!";
+
+var boo = true;
+var lean = false;
+
+console.log(pi,"<--That's pi...mostly")
+console.log("The phone number my parents had when I was a kid:", phone)
+console.log("Everyone has a favorite:", favorite)
+console.log("It's the square root of two! -->", root)
+
+console.log("Everyone wants to be more than they are.", string)
+console.log("What about those who value having over being?", string2)
+console.log(yarn,"...only because you ate the cookies and became a fat string.")
+
+console.log(boo,"Ghosts say 'boo'.")
+console.log("If I eat all the cookies, I will get lean! : ", lean)
